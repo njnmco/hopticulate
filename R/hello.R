a8e01cb4465119ffe73134053981990cfac8ce07
Neal Fultz <nfultz@gmail.com>
2019-11-01 10:56:50 -0700

    Adding Math ops for pyll vars; adding getting started guide.

--- R/hello.R
+++ R/hello.R
@@ -17,7 +17,42 @@ delayedAssign("hyperopt", reticulate::import("hyperopt", delay_load = TRUE))
 
 
 for(i in c('hp', 'fmin', 'tpe'))
-  delayedAssign(i, try(hyperopt[[i]]), eval.env = list2env(list(i=i)))
+  delayedAssign(i, hyperopt[[i]], eval.env = list2env(list(i=i)))
 
 
 rm(i)
+
+
+Ops.hyperopt.pyll.base.Apply <- function(e1, e2) {
+  left  <- inherits(e1, "hyperopt.pyll.base.Apply")
+  right <- inherits(e2, "hyperopt.pyll.base.Apply")
+
+  if(!left && ! right){
+    stop("This should be impossible")
+  }
+  else if(left) {
+    m <- switch(
+      .Generic,
+      "+"=as.name("__add__"),
+      "-"=as.name("__sub__"),
+      "*"=as.name("__mul__"),
+      "/"=as.name("__div__"),
+      stop("Unsupported operator for hyperopt:", .Generic))
+
+    x <- e1
+    y <- e2
+  }
+  else if (right) {
+    m <- switch(
+      .Generic,
+      "+"=as.name("__radd__"),
+      "-"=as.name("__rsub__"),
+      "*"=as.name("__rmul__"),
+      "/"=as.name("__rdiv__"),
+      stop("Unsupported operator for hyperopt:", .Generic))
+    x <- e2
+    y <- e1
+  }
+
+  eval(substitute(x$m(y)))
+}
---
+++ demo/getting-started.R
@@ -0,0 +1,31 @@
+#' ## Getting Started
+#'
+#' This is a literal transcription of the getting started example from
+#' http://hyperopt.github.io/hyperopt/
+#'
+
+# Load the package:
+library(hopticulate)
+
+# define an objective function
+objective <- function(args){
+  case <- args[[1]]; val <- args[[2]]
+  if (case == 'case 1')
+    return(val)
+  else
+    return(val ** 2)
+}
+
+# Define a search space.
+space = hp$choice('a',
+                    list(
+                      list('case 1', hp$lognormal('c1', 0, 1) + 1),
+                      list('case 2', hp$uniform('c2', -10, 10))
+                    )
+                  )
+
+best = fmin(objective, space, algo=tpe$suggest, max_evals=100)
+
+print(best)
+# -> {'a': 1, 'c2': 0.01420615366247227}
+print(hyperopt$space_eval(space, best))
