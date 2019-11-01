0983537149c23e09fb22433173fc6b3e8766be89
Neal Fultz <nfultz@gmail.com>
2019-11-01 09:51:39 -0700

    Initial commit.

---
+++ R/hello.R
@@ -0,0 +1,23 @@
+# Hello, world!
+#
+# This is an example function named 'hello'
+# which prints 'Hello, world!'.
+#
+# You can learn more about package authoring with RStudio at:
+#
+#   http://r-pkgs.had.co.nz/
+#
+# Some useful keyboard shortcuts for package authoring:
+#
+#   Install Package:           'Ctrl + Shift + B'
+#   Check Package:             'Ctrl + Shift + E'
+#   Test Package:              'Ctrl + Shift + T'
+
+delayedAssign("hyperopt", reticulate::import("hyperopt", delay_load = TRUE))
+
+
+for(i in c('hp', 'fmin', 'tpe'))
+  delayedAssign(i, try(hyperopt[[i]]), eval.env = list2env(list(i=i)))
+
+
+rm(i)
